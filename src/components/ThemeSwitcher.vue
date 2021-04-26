6cf6964fb08f0ef5723d2cebb269e13385345f26
NMaXIMuSN <58858943+NMaXIMuSN@users.noreply.github.com>
2021-04-26 19:50:56 +0300

    [CRM-185]chore: update-theme-slider (#18)
    [CRM-185]chore: update-theme-slider

--- src/components/ThemeSwitcher.vue
+++ src/components/ThemeSwitcher.vue
@@ -1,25 +1,16 @@
 <template>
-  <label class="switcher" :class="{ dark: IS_THEME_DARK }">
-    <input type="checkbox" @change="setIsThemeDark(!IS_THEME_DARK)" />
-    <div class="switcher__img">
-      <img
-        :src="themeImage"
-        alt="Светлая тема"
-        :class="{ dark: IS_THEME_DARK }"
-      />
-    </div>
-  </label>
+  <div class="switcher">
+    <label>
+      <input type="checkbox" @change="setIsThemeDark(!IS_THEME_DARK)"  />
+      <span :class="{ dark: IS_THEME_DARK }"></span>
+    </label>
+  </div>
 </template>
 
 <script>
 import { mapMutations, mapState } from "vuex";
 export default {
   computed: {
-    themeImage() {
-      return this.IS_THEME_DARK
-        ? require("@/assets/img/moon.svg")
-        : require("@/assets/img/sun.svg");
-    },
     ...mapState({ IS_THEME_DARK: (state) => state.isThemeDark }),
   },
   methods: mapMutations(["setIsThemeDark"]),
@@ -27,35 +18,73 @@ export default {
 </script>
 <style lang="scss" scoped>
 .switcher {
-  position: relative;
-  width: 60px;
-  height: 35px;
-  padding: 2px 0 2px 2px;
-  border: 2px solid #0b2755;
-  border-radius: 60px;
-  cursor: pointer;
-  transition: 0.3s cubic-bezier(0.33, 1, 0.32, 1);
-
-  &.dark {
-    background-color: #0b2755;
-    transition: 0.3s cubic-bezier(0.33, 1, 0.32, 1);
-  }
-  &__img {
-    display: flex;
-    align-items: center;
-  }
   input {
-    position: absolute;
-    z-index: -1;
-    opacity: 0;
+    display: none;
   }
-  img {
-    width: 28px;
+  span.dark {
+    border-color: #333;
+    background: #2c4e63;
+    &:before {
+      left: 55%;
+      box-shadow: inset -5px 0rem 0 0.4px #ffc207;
+      background: transparent;
+    }
+    &:after {
+      content: "★";
+      top: 4px;
+      left: 26px;
+      width: 10px;
+      height: 10px;
+      font-size: 10px;
+      color: #ffc207;
+    }
+  }
+  
+  span {
+    cursor: pointer;
+    position: relative;
+    display: inline-block;
+    vertical-align: middle;
+    width: 55px;
     height: 28px;
-    transition: 0.3s cubic-bezier(0.33, 1, 0.32, 1);
-    &.dark {
-      transition: 0.3s cubic-bezier(0.33, 1, 0.32, 1);
-      transform: translateX(100%);
+    border-radius: 60px;
+    border: 2px solid #666;
+    background: #5197c4;
+    transition: all 0.5s cubic-bezier(0.34, 1.56, 0.64, 1);
+    &:before {
+      content: '';
+      position: absolute;
+      top: 2px;
+      left: 3px;
+      width: 20px;
+      height: 20px;
+      border-radius: 50%;
+      box-shadow: inset -4rem -4rem 0 0 #ffc207;
+      transition: all 0.5s cubic-bezier(0, 0.55, 0.45, 1);
+      &.dark{
+        left: 55%;
+        box-shadow: inset -5px 0rem 0 0.4px #ffc207;
+      }
+    }
+    &:after {
+      content: '\002601';
+      position: absolute;
+      top: 4px;
+      left: 10px;
+      width: 1px;
+      height: 1px;
+      color: #fff;
+      font-size: 20px;
+      transition: all 0.5s cubic-bezier(0, 0.55, 0.45, 1);
+      &.dark{
+        content: "★";
+        top: 2px;
+        left: 23px;
+        width: 10px;
+        height: 10px;
+        font-size: 10px;
+        color: #ffc207;
+      }
     }
   }
 }
